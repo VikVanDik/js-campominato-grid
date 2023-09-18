c9b21c9e470ad8a91c92a30c48bed3da1171274d
VikVanDik <95vlc17@gmail.com>
2023-09-18 16:15:22 +0200

    [bug] : numcell

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
 
+// 2. Creare un ciclo al quale collegare gli elementi da creare (da aggiungere a funzione?).
+
+const container = document.querySelector('.c-container')
+
+function createCells (numCell) {
+  for (let i = 1; i <=numCell; i++) {
+    let cell = document.createElement ('div')
+    cell.classList.toggle ('cell-numCell')
+    container.append(cell);
+  }
+}
+
+
+console.log(container);
+
