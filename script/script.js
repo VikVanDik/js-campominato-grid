a5eb0ff52f9f1f57c7344a18d36563f962d29f2c
VikVanDik <95vlc17@gmail.com>
2023-09-18 19:11:36 +0200

    [fix] : repeat bug

--- script/script.js
+++ script/script.js
@@ -6,43 +6,39 @@ const container = document.querySelector('.c-container')
 // 2. Creare un ciclo al quale collegare gli elementi da creare (da aggiungere a funzione).
 function createCells (numCell) {
   for (let i = 1; i <=numCell; i++) {
-    // 3. Creare gli elementi da javascript ed aggiungere loro le classi necessarie.
     let cell = document.createElement ('div')
-    cell.classList.toggle(`cell-${numCell}`)
+    // 3. Creare gli elementi da javascript ed aggiungere loro le classi necessarie.
+    cell.classList.add(`cell-${numCell}`)
     container.append(cell);
-
     cell.addEventListener ('click', function () {
       let classAdded = this.classList.toggle ('clicked')
-      
+
       if (classAdded) {
         cell.innerHTML = [i]
       } else {
         cell.innerHTML = ``
       }
+      
     })
-  }
-  
-}
-
-
-function difficulty (){
-  const select = document.getElementById ('difficulty').value
-  if (select === 1){
-    createCells(100)
-  } else if (select === 2){
-    createCells(81)
-  } else {
-    createCells(49)
   }
 }
 
 const button = document.getElementById('button')
-
-button.addEventListener('click', function (){
+button.addEventListener('click', function(){
+  container.innerHTML = ``
   container.classList.remove ('d-none')
   difficulty();
-  console.log(select);
 })
 
 console.log(container);
-
+function difficulty (){
+  const select = document.getElementById ('difficulty').value
+  console.log(select);
+  if (select == 1){
+    createCells(100);
+  } else if (select == 2){
+    createCells(81);
+  } else if (select == 3) {
+    createCells(49);
+  }
+}
